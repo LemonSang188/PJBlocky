10e2095d480f7c1c9cd17552acf7ff578fc34160
DESKTOP-500QATH\LENOVO <koonjikung01@hotmail.com>
2025-09-02 20:11:47 +0700

    commitv1

--- blocklyduino/arduino/addon/custom.js
+++ blocklyduino/arduino/addon/custom.js
@@ -6,9 +6,12 @@ goog.require('Blockly.Arduino');
 
 
 Blockly.Arduino['LedRGB'] = function(block) {
-  Blockly.Arduino.setups_['RGBR'] = 'const int RGB_R = 9';
-  Blockly.Arduino.setups_['RGBG'] = 'const int RGB_G = 10';
-  Blockly.Arduino.setups_['RGBB'] = 'const int RGB_B = 11';
+  Blockly.Arduino.definitions_['defieRGBR'] = 'const int RGB_R = 9;';
+  Blockly.Arduino.definitions_['defieRGBG'] = 'const int RGB_G = 10;';
+  Blockly.Arduino.definitions_['defieRGBB'] = 'const int RGB_B = 11;';
+  Blockly.Arduino.setups_['RGBR'] = 'pinMode(RGB_R, OUTPUT);';
+  Blockly.Arduino.setups_['RGBG'] = 'pinMode(RGB_G, OUTPUT);';
+  Blockly.Arduino.setups_['RGBB'] = 'pinMode(RGB_B, OUTPUT);';
 
   var ledRGB  = block.getFieldValue('ledRGB');
   var ledState = block.getFieldValue('ledState');
@@ -71,6 +74,14 @@ Blockly.Arduino['MhMqSensor'] = function() {
   return code;
 };
 
+Blockly.Arduino['MhMqSensorInput'] = function() {
+  Blockly.Arduino.definitions_['MQdefie1'] = '#define MQ2_ANALOG A1;';
+  Blockly.Arduino.definitions_['MQdefie2'] = '#define MQ2_DIGITAL 2;';
+  Blockly.Arduino.setups_['MQPin'] = 'pinMode(MQ2_DIGITAL, INPUT);';
+  var code = `analogRead(MQ2_ANALOG)`;
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
 Blockly.Arduino['HcSr04'] = function() {
   Blockly.Arduino.definitions_['HCdefie1'] = '#define ECHO_PIN 1';
   Blockly.Arduino.definitions_['HCdefie2'] = '#define TRIG_PIN 3'
@@ -95,8 +106,30 @@ Blockly.Arduino['HcSr04'] = function() {
   return code;
 };
 
+Blockly.Arduino['HcSr04Input'] = function() {
+  Blockly.Arduino.definitions_['HCdefie1'] = '#define ECHO_PIN 1';
+  Blockly.Arduino.definitions_['HCdefie2'] = '#define TRIG_PIN 3'
+  Blockly.Arduino.setups_['HCPin1'] = 'pinMode(ECHO_PIN, INPUT);';
+  Blockly.Arduino.setups_['HCPin2'] = 'pinMode(TRIG_PIN, OUTPUT);';
+  Blockly.Arduino.definitions_['ultrasonic_func'] = `
+  long ultrasonicRead(int TRIG_PIN, int ECHO_PIN) {
+  pinMode(TRIG_PIN, OUTPUT);
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  pinMode(ECHO_PIN, INPUT);
+  return pulseIn(ECHO_PIN, HIGH) * 0.034 / 2;
+  }`;
+
+  var code = 'ultrasonicRead(TRIG_PIN,ECHO_PIN )';
+
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
 Blockly.Arduino['52abLm35Dz'] = function() {
-  Blockly.Arduino.definitions_['52abdefie1'] = '#define LM35_PIN A0 ECHO_PIN 3';
+  Blockly.Arduino.definitions_['52abdefie1'] = '#define LM35_PIN A0';
   Blockly.Arduino.definitions_['52abdefie2'] = 'int tempValue = 0;';
   Blockly.Arduino.definitions_['52abdefie3'] = 'float temperatureC = 0.0;';
   Blockly.Arduino.setups_['52abdefiePin'] = "// LM35 doesn't need pinMode because analogRead handles it";
@@ -112,8 +145,15 @@ Blockly.Arduino['52abLm35Dz'] = function() {
   return code;
 };
 
+Blockly.Arduino['52abLm35DzInput'] = function() {
+  Blockly.Arduino.definitions_['52abdefie1'] = '#define LM35_PIN A0';
+  Blockly.Arduino.setups_['52abdefiePin'] = "// LM35 doesn't need pinMode because analogRead handles it";
+  var code = `analogRead(ECHO_PIN) * 0.48828125`;
+  return code;
+};
+
 Blockly.Arduino['Mg996r'] = function(block) {
-  Blockly.Arduino.definitions_['MGdefie'] = '#define Servo_PIN ยังไม่ได้ตั้ง';
+  Blockly.Arduino.definitions_['MGdefie'] = '#define Servo_PIN 6';
   Blockly.Arduino.setups_['MGPin'] = 'Myservo.attach(Servo_PIN);';
   Blockly.Arduino.includes_['includes_servo'] = '#include <Servo.h>';
 
@@ -129,10 +169,10 @@ Blockly.Arduino['delay'] = function(block) {
 };
 
 Blockly.Arduino['L9110'] = function(block) {
-  Blockly.Arduino.definitions_['L9defie1'] = '#define L9110A1A ยังไม่ได้ตั้ง';
-  Blockly.Arduino.definitions_['L9defie2'] = '#define L9110A1B ยังไม่ได้ตั้ง';
-  Blockly.Arduino.definitions_['L9defie3'] = '#define L9110B1A ยังไม่ได้ตั้ง';
-  Blockly.Arduino.definitions_['L9defie4'] = '#define L9110B1B ยังไม่ได้ตั้ง';
+  Blockly.Arduino.definitions_['L9defie1'] = '#define L9110A1A 7';
+  Blockly.Arduino.definitions_['L9defie2'] = '#define L9110A1B 8';
+  Blockly.Arduino.definitions_['L9defie3'] = '#define L9110B1A 12';
+  Blockly.Arduino.definitions_['L9defie4'] = '#define L9110B1B 13';
 
   Blockly.Arduino.setups_['L9Setup1'] = 'pinMode(L9110A1A, OUTPUT);';
   Blockly.Arduino.setups_['L9Setup2'] = 'pinMode(L9110A1B, OUTPUT);';
